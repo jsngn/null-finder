19dc8da1092064f0a0685839f7c302bb4c488ed8
jsngn <mien.t.nguyen.22@dartmouth.edu>
2020-04-23 00:08:16 -0400

    Reads each field of CSV

--- find_null.c
+++ find_null.c
@@ -13,6 +13,7 @@
 int validate_args(int argc, char *argv[]);
 int read_nulls(char *file, hashtable_t *null_words);
 int read_csv(char *argv[]);
+char *tokenize(char *str);
 
 int main(int argc, char *argv[])
 {
@@ -92,7 +93,7 @@ int read_csv(char *argv[])
 	hashtable_t **columns; // Each hashtable in array reps a column, in each column table key is val, val is freq
 	hashtable_t *column_to_nulls; // Each key is column, val is char **array of possible null values
 	int line_len = 1000; // Starter size
-	char *line = calloc(line_len, sizeof(char));
+	char *line = malloc(line_len * sizeof(char));
 	*line = '\0'; // Safety
 	int c;
 
@@ -105,7 +106,7 @@ int read_csv(char *argv[])
 	read_nulls(argv[1], null_words);
 
 	hashtable_print(null_words);	
-
+	
 	// Read from csv
 	if ((fp = fopen(argv[2], "r")) == NULL) {
 		return 4;
@@ -114,32 +115,54 @@ int read_csv(char *argv[])
 	int col_count = 0;
 	while ((c = fgetc(fp)) != EOF) {
 		if (c == '\n') { // We've got a full line
-			if (strlen(word) > 0) { // In case multiple newlines between lines
-				if (col_count == 0) { // 1st line/column line
+			if (strlen(line) > 0) { // In case multiple newlines between lines
+				char **tokens; 
+				char *token;
+				char *curr_line = line;
+				int true_len = strlen(line); // tokenize adds \0 so store it before
+				int curr_len = 0;
+				token = tokenize(curr_line);
+				
+				while (token != NULL) {
+					printf("\n%s", token);
+					curr_len += (strlen(token) + 1);
+					if (curr_len >= true_len) {
+						break;
+					}
+					curr_line += (strlen(token) + 1);
+					token = tokenize(curr_line);
+				}
+				//if (col_count == 0) { // 1st line/column line
 					// split line string by comma, set col_count to length, initialize hashtable**
-				}
-				hashtable_insert(null_words, word, dummy);
-				dummy = calloc(1, sizeof(char));
-				if (dummy == NULL) {
+
+				//}
+				//hashtable_insert(null_words, word, dummy);
+				//dummy = calloc(1, sizeof(char));
+				//if (dummy == NULL) {
+				//	fclose(fp);
+				//	return 4;
+				//}
+				free(line);
+				line_len = 1000;
+				line = malloc(line_len * sizeof(char));
+				if (line == NULL) {
 					fclose(fp);
+				//	free(dummy);
 					return 4;
 				}
-				word = calloc(50, sizeof(char));
-				if (word == NULL) {
-					fclose(fp);
-					free(dummy);
-					return 4;
-				}
-				*word = '\0';
+				*line = '\0';
 			}
 		}
 		else {
 			int len_so_far = strlen(line);
 
 			// Expand if needed
-			if (len_so_far + 1 > line_len) {
+			if (len_so_far + 2 > line_len) {
 				line_len = line_len * 2;
-				line = realloc(line_len, sizeof(char));
+				line = realloc(line, line_len*sizeof(char));
+				if (line == NULL) {
+					return 4;
+				}
 				*(line + len_so_far) = '\0';
 			} 
 
@@ -155,4 +178,40 @@ int read_csv(char *argv[])
 	hashtable_free(null_words);
 
 	return 0;
+}
+
+char *tokenize(char *str)
+{
+	static char *next = NULL;
+	if (str) {
+		next = str;
+	}
+	
+	//if(!*next) {
+		//printf("%s\n", next);
+//		return NULL;
+//	}
+        
+	str = next;
+
+	if(*str == '"') {
+		str++;
+	        next++;
+		while(*next && *next != '"') {
+			next++;
+		}
+		if(*next == '"') {
+			*next++ = '\0';
+		}
+	}
+
+	while(*next && *next != ',') {
+		next++;
+	}
+
+	if(*next) {
+		*next++ = '\0';
+	}
+				       
+       	return str;
 }
